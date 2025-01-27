4ce75f671e9e4125ca73d68e3ab23f40d33e07f9
unknown <mm0832556@gmail.com>
2025-01-27 15:27:52 +0200

    tap bar

--- lib/feature/home/presentation/pages/home_page.dart
+++ lib/feature/home/presentation/pages/home_page.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/themes/app_colors.dart';
 import 'package:movie_app/core/themes/text_style.dart';
-import 'package:movie_app/feature/home/presentation/widgets/bottom_bar.dart';
 import 'package:movie_app/feature/home/presentation/widgets/movie.dart';
 
 class HomePage extends StatelessWidget {
